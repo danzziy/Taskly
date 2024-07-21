6a491912f9030995054995d43b008f4eb6d45236
danzziy <dannyazzawi@gmail.com>
2024-07-21 19:49:14 -0400

    fix format

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -225,56 +225,56 @@ class HomePage extends StatelessWidget {
 
   Container _searchField() {
     return Container(
-          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-          decoration: BoxDecoration(
-            boxShadow:[
-               BoxShadow(
-                color: Color(0xff1d1617).withOpacity(0.11),
-                blurRadius: 40,
-                spreadRadius: 0.0,
-              ),
-            ],
+      margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+      decoration: BoxDecoration(
+        boxShadow:[
+            BoxShadow(
+            color: Color(0xff1d1617).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0.0,
           ),
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              contentPadding: EdgeInsets.all(15),
-              hintText: "Search Pancake",
-              hintStyle: TextStyle(
-                color: Colors.grey
-              ),
-              prefixIcon: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: SvgPicture.asset('assets/food_tutorial/Search.svg'),
-              ),
-              suffixIcon: Container(
-                width: 100,
-                child: IntrinsicHeight(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      VerticalDivider(
-                        color: Color(0x0),
-                        indent: 10,
-                        endIndent: 10,
-                        thickness: 0.1,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: SvgPicture.asset('assets/food_tutorial/Filter.svg'),
-                      ),
-                    ],
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.all(15),
+          hintText: "Search Pancake",
+          hintStyle: TextStyle(
+            color: Colors.grey
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: SvgPicture.asset('assets/food_tutorial/Search.svg'),
+          ),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  VerticalDivider(
+                    color: Color(0x0),
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: SvgPicture.asset('assets/food_tutorial/Filter.svg'),
+                  ),
+                ],
               ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: BorderSide.none,
-              )
             ),
           ),
-        );
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          )
+        ),
+      ),
+    );
   }
 
   AppBar appBar() {
